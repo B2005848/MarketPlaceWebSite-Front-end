870b3d75a334a4e25428318ac0bb1741c088751b
B2005848 <namltgt2002@gmail.com>
2023-11-28 05:54:49 +0700

    ook

---
+++ src/components/Catelogies.homepage.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const catadata = ref([]);
+const getData = async () => {
+  try {
+    const response = await window.axios.get(
+      "http://localhost:3000/api/catagoies/getall"
+    );
+    catadata.value = response.data.data;
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+onMounted(() => {
+  getData();
+});
+</script>
+<template>
+  <div class="mb-5 ms-5"><h3>PRODUCT LIST</h3></div>
+
+  <div class="row">
+    <div
+      v-for="(catadata, index) in catadata"
+      :key="index"
+      class="col-sm-2 mb-4"
+    >
+      <div class="card text-center">
+        <div>
+          <img src="" alt="IMG" />
+        </div>
+        <div>
+          <a href="">{{ catadata.CategoryName }}</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: black;
+}
+
+.card {
+  padding: 10px;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+  left: 50%;
+  height: 100px;
+}
+
+.card:hover {
+  border: 0.2px #ef4565 solid;
+  transform: translateY(-2px);
+  transition: ease-out;
+}
+</style>
--- src/layouts/homepage.vue
+++ src/layouts/homepage.vue
@@ -3,12 +3,41 @@
     <theHeader />
   </header>
 
-  <div class="mt-3">
-    <div class="main">
+  <div class="mt-3 main">
+    <div>
       <!-- img banner -->
-      <theBanner />
+      <div style="padding: 20px">
+        <theBanner />
+      </div>
+      <!-- CATALOG -->
+      <div class="mt-5">
+        <theCateLog />
+      </div>
 
-      <div class="p-2 mt-5 border-top">
+      <!-- LIST PRODUCTS -->
+      <hr
+        style="
+          font-weight: 700;
+          box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+            rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+            rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+        "
+      />
+      <div class="p-2 pe-5 ps-5 mt-5">
+        <h2
+          class="mb-5"
+          style="
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            font-weight: 500;
+            color: #ff6e6c;
+            box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
+              rgba(179, 122, 122, 0.22) 0px 15px 12px;
+            border-radius: 13px;
+            padding: 15px;
+          "
+        >
+          TODAY'S PROPOSAL
+        </h2>
         <theMain />
       </div>
     </div>
@@ -27,6 +56,7 @@ import theHeader from "@/components/HomePageHeader.vue";
 import theBanner from "@/components/banner.homepage.vue";
 import theFooter from "@/components/Footer.homepage.vue";
 import theMain from "@/components/Body.homepage.vue";
+import theCateLog from "@/components/Catelogies.homepage.vue";
 </script>
 
 <style scoped>
@@ -36,5 +66,6 @@ import theMain from "@/components/Body.homepage.vue";
   left: 50%;
   transform: translateX(-50%);
   background-color: aliceblue;
+  border-radius: 13px;
 }
 </style>

f3fa0cab61e7481ceb1cd6fda932495652be7a6c
B2005848 <namltgt2002@gmail.com>
2023-11-29 15:21:44 +0700

    ok nha

--- src/layouts/homepage.vue
+++ src/layouts/homepage.vue
@@ -1,55 +1,58 @@
 <template>
-  <header>
-    <theHeader />
-  </header>
+  <div style="margin: 0 auto; padding: 0">
+    <header>
+      <theHeader />
+    </header>
 
-  <div class="mt-3 main">
-    <div>
-      <!-- img banner -->
-      <div style="padding: 20px">
-        <theBanner />
-      </div>
-      <!-- CATALOG -->
-      <div class="mt-5">
-        <theCateLog />
-      </div>
+    <div class="mt-3 main">
+      <div>
+        <!-- img banner -->
+        <div style="padding: 20px">
+          <theBanner />
+        </div>
+        <!-- CATALOG -->
+        <div class="mt-5">
+          <theCateLog />
+        </div>
 
-      <!-- LIST PRODUCTS -->
-      <hr
-        style="
-          font-weight: 700;
-          box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
-            rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
-            rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-        "
-      />
-      <div class="p-2 pe-5 ps-5 mt-5">
-        <h2
-          class="mb-5"
+        <!-- LIST PRODUCTS -->
+        <hr
           style="
-            font-family: Verdana, Geneva, Tahoma, sans-serif;
-            font-weight: 500;
-            color: #ff6e6c;
-            box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
-              rgba(179, 122, 122, 0.22) 0px 15px 12px;
-            border-radius: 13px;
-            padding: 15px;
+            font-weight: 700;
+            box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+              rgba(0, 0, 0, 0.12) 0px -12px 30px,
+              rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px,
+              rgba(0, 0, 0, 0.09) 0px -3px 5px;
           "
-        >
-          TODAY'S PROPOSAL
-        </h2>
-        <theMain />
+        />
+        <div class="p-2 pe-5 ps-5 mt-5">
+          <h2
+            class="mb-5"
+            style="
+              font-family: Verdana, Geneva, Tahoma, sans-serif;
+              font-weight: 500;
+              color: #ff6e6c;
+              box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
+                rgba(179, 122, 122, 0.22) 0px 15px 12px;
+              border-radius: 13px;
+              padding: 15px;
+            "
+          >
+            TODAY'S PROPOSAL
+          </h2>
+          <theMain />
+        </div>
       </div>
     </div>
+
+    <router-view></router-view>
+
+    <hr />
+
+    <footer class="mt-5">
+      <theFooter />
+    </footer>
   </div>
-
-  <router-view></router-view>
-
-  <hr />
-
-  <footer class="mt-5">
-    <theFooter />
-  </footer>
 </template>
 <script setup>
 import theHeader from "@/components/HomePageHeader.vue";
@@ -65,7 +68,7 @@ import theCateLog from "@/components/Catelogies.homepage.vue";
   position: relative;
   left: 50%;
   transform: translateX(-50%);
-  background-color: aliceblue;
+  background-color: rgb(255, 243, 245);
   border-radius: 13px;
 }
 </style>

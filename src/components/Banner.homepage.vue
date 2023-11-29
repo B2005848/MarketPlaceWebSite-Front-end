338ea51ab1e52644d2b69f46a28f668d8ecf61ba
B2005848 <namltgt2002@gmail.com>
2023-11-29 15:12:39 +0700

    ok

--- src/components/Banner.homepage.vue
+++ src/components/Banner.homepage.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="d-flex justify-content-center">
     <div class="me-3">
-      <img src="../assets/bacgr.jpg" alt="" />
+      <img
+        src="https://plus.unsplash.com/premium_photo-1678752713875-17fe3fd98ba3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
     </div>
 
     <div class="item-header-3">
@@ -9,7 +12,7 @@
         <div class="carousel-inner">
           <div class="carousel-item active" data-bs-interval="900">
             <img
-              src="https://images.pexels.com/photos/3962285/pexels-photo-3962285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src="https://images.unsplash.com/photo-1519643381401-22c77e60520e?q=80&w=873&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               class="d-block custom-image"
               alt="Term"
             />
@@ -17,7 +20,7 @@
 
           <div class="carousel-item active" data-bs-interval="900">
             <img
-              src="https://images.pexels.com/photos/5650052/pexels-photo-5650052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src="https://images.unsplash.com/photo-1519643225200-94e79e383724?q=80&w=876&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               class="d-block custom-image"
               alt="Term"
             />
@@ -25,7 +28,7 @@
 
           <div class="carousel-item active" data-bs-interval="1000">
             <img
-              src="https://images.pexels.com/photos/2720447/pexels-photo-2720447.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src="https://images.unsplash.com/photo-1497366216548-37526070297c?q=80&w=869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               class="d-block custom-image"
               alt="Term"
             />
@@ -33,7 +36,7 @@
 
           <div class="carousel-item active" data-bs-interval="900">
             <img
-              src="https://images.pexels.com/photos/2170450/pexels-photo-2170450.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src="https://images.unsplash.com/photo-1511389026070-a14ae610a1be?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               class="d-block custom-image"
               alt="Term"
             />
@@ -41,7 +44,7 @@
 
           <div class="carousel-item active" data-bs-interval="800">
             <img
-              src="https://images.pexels.com/photos/5650050/pexels-photo-5650050.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src="https://plus.unsplash.com/premium_photo-1678402545077-7a9ec2b5e5b8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               class="d-block custom-image"
               alt="Term"
             />
